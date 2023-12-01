01cd1fbf1b80d336a17dee4d0d154048bd0e65d6
lipeedev <anamariafakezaum@gmail.com>
2023-12-01 16:18:40 +0000

    refactor: change chat messages style

--- web/src/components/ChatMessage.tsx
+++ web/src/components/ChatMessage.tsx
@@ -27,8 +27,8 @@ export function ChatMessage({ message, animate = true, isSender, isServer = fals
     immediate: !animate,
   });
 
-  const colorSender = `bg-gradient-to-l from-[#D99F84] to-[#F2D6C9] text-[#D99F84] ${!isLastMsgFromSender ? 'rounded-tl-2xl rounded-bl-2xl rounded-br-2xl' : 'rounded-2xl'}`
-  const colorReceiver = `${isServer ? 'text-[#F2D6C9] bg-gradient-to-l from-teal-700/40  to-emerald-900/40' : 'bg-[#7D8074]/80 text-[#F2D6C9]'} ${(!isLastMsgFromSender && !isServer) ? 'rounded-bl-2xl rounded-br-2xl rounded-tr-2xl' : 'rounded-2xl'}`
+  const colorSender = `bg-gradient-to-l from-[#D99F84] to-[#F2D6C9] text-[#D99F84] ${!isLastMsgFromSender ? 'rounded-tl-full rounded-bl-full rounded-br-full' : 'rounded-full'}`
+  const colorReceiver = `${isServer ? 'text-[#F2D6C9] bg-gradient-to-l from-teal-700/40  to-emerald-900/40' : 'bg-[#7D8074]/80 text-[#F2D6C9]'} ${(!isLastMsgFromSender && !isServer) ? 'rounded-bl-full rounded-br-full rounded-tr-full' : 'rounded-full'}`
   if (isServer) {
     message.sender = message.sender[0].toUpperCase() + message.sender.slice(1)
   }
@@ -38,13 +38,13 @@ export function ChatMessage({ message, animate = true, isSender, isServer = fals
       {(!isServer && !isLastMsgFromSender) && <img referrerPolicy="no-referrer" src={message.profileImage ?? "/default.png"} className="h-9 w-9 rounded-full" />}
       {(!isServer && isLastMsgFromSender) && <div className="w-9" />}
 
-      <div className={`${isSender ? colorSender : colorReceiver} py-2 px-3 flex flex-col gap-1 ${message.isAudio && 'w-full'}`}>
+      <div className={`${isSender ? colorSender : colorReceiver} py-2 px-4 flex flex-col gap-1 ${message.isAudio && 'w-full'}`}>
         <div className="flex justify-between">
-          {!isSender && <span className='font-bold text-md'>{message.sender}</span>}
+          {(!isSender && !isLastMsgFromSender) && <span className='font-bold text-md'>{message.sender}</span>}
           {isServer && <BadgeInfo className="text-[#D99F84]" />}
         </div>
 
-        <div className="flex justify-betwe">
+        <div className="mx-1 flex justify-between">
           {
             !message.isAudio
               ? <p className={`pr-4 break-all ${!isSender ? 'text-[#F2E5E4]' : 'text-[#613829]'}`}>{message.content.trim()}</p>
